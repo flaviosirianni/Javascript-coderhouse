905c0a38d3370b2532417260118d0588f6656ca1
flavioSirianni <flaviosirianni@gmail.com>
2021-02-03 14:57:42 -0300

    Mejoras funcionales

--- Proyecto/1er Entrega Proyecto Final/simuladorPrestamos.js
+++ Proyecto/1er Entrega Proyecto Final/simuladorPrestamos.js
@@ -1,9 +1,22 @@
 var mesesPrestamo = [12, 24, 36, 48, 60, 72];
 var interesAnual = 0.66 // TNA = 66%
 var cantAnios = [1,2,3,4,5];
-var maximoPrestable = 0;
+var cuotasMaximas = [];
 
-function calculoCuotaMaxMensual(){
+function guardarLocal(){
+    localStorage.setItem("ultimaBusqueda", JSON.stringify(cuotasMaximas));
+    localStorage.setItem("sueldo", JSON.stringify(Number(document.getElementById("sueldo").value)));
+    localStorage.setItem("gastos", JSON.stringify(Number(document.getElementById("gastos").value)));
+}
+
+function usarMemoria(){
+    let sueldo = document.getElementById("sueldo");
+    let gastos = document.getElementById("gastos");
+    sueldo.value = localStorage.getItem("sueldo");
+    gastos.value = localStorage.getItem("gastos");
+}
+
+function calculoCuotaMaxMensual(){ // Funcion auxiliar de calculoPrestamoMax()
     let sueldo = Number(document.getElementById("sueldo").value);
     let gastos = Number(document.getElementById("gastos").value);
     let cuotaMaxMensual = (sueldo - gastos) / 2;
@@ -15,20 +28,20 @@ function calculoPrestamoMax(){
     let sueldo = Number(document.getElementById("sueldo").value);
     let gastos = Number(document.getElementById("gastos").value);
     let a = 12 * calculoCuotaMaxMensual();
-    
     if(a>0){
         if((sueldo/gastos)>=2){
+            cuotasMaximas = [];
             for(let i = 0; i<cantAnios.length; i++){
                     coMax.push((a / ( interesAnual / ( 1 - ((1 + interesAnual)**(-cantAnios[i])) ))).toFixed(0)); // Cálculo interés Francés -> cuotaAnual = dineroPrestado * ( interesAnual / (1 - (1 + interesAnual)^-cantAnios))
+                    cuotasMaximas.push({cantMeses: (cantAnios[i]*12), maxPrestable: coMax[i]});     
                 }
-            let aux2 = document.getElementById("opcionesPrestamos"); //Esto verifica que no haya otros valores ya mostrados en pantalla.
-            if(aux2 == null){
+            let aux = document.getElementById("opcionesPrestamos"); // Verifica que no haya otros valores ya mostrados en pantalla.
+            if(aux == null){
                 mostrarValores(coMax); 
             } else {
                 borrarValores("opcionesPrestamos");
                 mostrarValores(coMax);
             }
-            return coMax;
         } else if((sueldo/gastos)<0){
             swal("Estás poniendo valores negativos..");
             borrarValores("opcionesPrestamos");
@@ -43,6 +56,7 @@ function calculoPrestamoMax(){
 }   
 
 function mostrarValores(coMax2){ 
+    let maximoPrestable = 0;
     for(let i = 0; i < coMax2.length; i++){
         let nuevoDiv = document.createElement("div");
         nuevoDiv.setAttribute("id", "opcionesPrestamos");
@@ -52,7 +66,8 @@ function mostrarValores(coMax2){
         nodoPadre.insertBefore(nuevoDiv, null);
         maximoPrestable = coMax2[i];
     }
-    mostrarSlider();
+    mostrarCuotas();
+    mostrarSlider(maximoPrestable);
 }
 
 function borrarValores(elemento){    
@@ -66,14 +81,30 @@ function borrarValores(elemento){
     }
 }
 
-function mostrarSlider(){
-    if(document.getElementById("masOpciones").innerText = " "){
-        document.getElementById("masOpciones").innerHTML += `
-        <br/><br/>
-        <div> Necesitás otro importe? Seleccionalo a continuación. </div>          
-        <div> Mínimo $5.000.- Máximo ${maximoPrestable}.- </div>          
-        <input class="inputSlider" id="slider" name="amount" step="1000" type="range" min="5000" max="${maximoPrestable}" onchange="moverSlider()"/>
-        <!--<input type="button" class="botonesTipo1" value="Consultar" onclick="consultaEspecifica()"/>-->
+function mostrarCuotas(){
+    if(document.getElementById("masOpcionesCuotas").innerText = " "){
+        document.getElementById("masOpcionesCuotas").innerHTML += `
+        <br/>
+        <h4> Necesitás otro importe? Seleccionalo a continuación. </h4>          
+        <div>¿En cuántas cuotas querés devolver el dinero?
+            <select id="cantCuotas" onclick="calcularOpcion()">
+                <option value="12">12 Cuotas</option>
+                <option value="24">24 Cuotas</option>
+                <option value="36">36 Cuotas</option>
+                <option value="48">48 Cuotas</option>
+                <option value="60">60 Cuotas</option>
+            </select>
+            </div>
+        `;
+    }
+}    
+
+function mostrarSlider(maxSlider){
+    if(document.getElementById("masOpcionesSlider").innerText = " "){
+        document.getElementById("masOpcionesSlider").innerHTML += `
+        <div> Mínimo $1.000.- Máximo ${maxSlider}.-           
+            <input class="inputSlider" id="slider" name="amount" step="500" type="range" min="1000" max="${maxSlider}" onchange="moverSlider()"/>
+        </div>
         `;
     }
 }
@@ -88,3 +119,27 @@ function moverSlider(){
     resultado.appendChild(valorInput);
     nodoPadre.insertBefore(resultado,null);
 }
+
+
+function calcularOpcion(){
+    let aux = document.getElementById("cantCuotas").value;
+    let i = 0;   
+    switch(aux){
+        case '12':
+            mostrarSlider(cuotasMaximas[0].maxPrestable);
+            break;
+        case '24':
+            mostrarSlider(cuotasMaximas[1].maxPrestable);
+            break;
+        case '36':
+            mostrarSlider(cuotasMaximas[2].maxPrestable);
+            break;
+        case '48':
+            mostrarSlider(cuotasMaximas[3].maxPrestable);
+            break;
+        case '60':
+            mostrarSlider(cuotasMaximas[4].maxPrestable);
+            break;
+    }
+}
+
