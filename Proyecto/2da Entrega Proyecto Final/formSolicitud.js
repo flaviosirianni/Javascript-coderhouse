90fd8a16253d866324bfd125be3d39534fb96611
flavioSirianni <flaviosirianni@gmail.com>
2021-03-12 15:04:53 -0300

    Entrega Final Completa

--- Proyecto/2da Entrega Proyecto Final/formSolicitud.js
+++ Proyecto/2da Entrega Proyecto Final/formSolicitud.js
@@ -1,22 +1,21 @@
 let BDSolicitantes = [];
 
-function guardarForm(){
-    localStorage.setItem("nombre", JSON.stringify(document.getElementById("nombre").value));
-    localStorage.setItem("apellido", JSON.stringify(document.getElementById("apellido").value));
-    localStorage.setItem("dni", JSON.stringify(Number(document.getElementById("dni").value)));
-    localStorage.setItem("telefono", JSON.stringify(Number(document.getElementById("telefono").value)));
-    localStorage.setItem("mail", JSON.stringify(document.getElementById("mail").value));
-    
+function validarForm(){   
     if($("#nombre").val() && $("#apellido").val() && $("#dni").val() && $("#telefono").val() && $("#mail").val()){
         habilitarBoton();
     }
-
 }
 
 function finalizarProceso(){
     if($("#nombre").val() && $("#apellido").val() && $("#dni").val() && $("#telefono").val() && $("#mail").val()){
-        let nuevoSolicitante = new Solicitante($("#nombre").val(), $("#apellido").val(), $("#dni").val(), $("#telefono").val(), $("#mail").val());
-        BDSolicitantes.push(nuevoSolicitante);
+        let nuevaSolicitud = {
+            nombre: $("#nombre").val(),
+            apellido: $("#apellido").val(),
+            dni: $("#dni").val(),
+            telefono: $("#telefono").val(),
+            mail: $("#mail").val()
+        };
+        BDSolicitantes.push(nuevaSolicitud);
         swal("Muchas gracias, un asesor te estará contactando en las próximas 24hs para coordinar los detalles.");
         setTimeout(() => {window.close();}, 3500);
     } else{
@@ -28,12 +27,3 @@ function habilitarBoton(){
     $("#finalizar").removeAttr("disabled");    
 }
 
-class Solicitante {
-    constructor(nombre, apellido, dni, telefono, mail){
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.dni = dni;
-    this.telefono = telefono;
-    this.mail = mail;
-    }    
-}
